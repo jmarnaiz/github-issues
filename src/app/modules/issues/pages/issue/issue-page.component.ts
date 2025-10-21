504ee5a76761443d71d195a004e8693c56baafad
Juan Manuel Arnaiz Caballero <jmanuelarnaiz@gmail.com>
2025-10-21 12:35:53 +0200

    Basic skeleton with routes

---
+++ src/app/modules/issues/pages/issue/issue-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-issue-page',
+  imports: [],
+  templateUrl: './issue-page.component.html',
+})
+export default class IssuePageComponent {}
---
+++ src/app/modules/issues/pages/issues-list/issues-list-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'issues-list-page',
+  imports: [],
+  templateUrl: './issues-list-page.component.html',
+})
+export default class IssuesListPageComponent {}
