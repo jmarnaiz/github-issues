a5441c9f32e58b26156085dc16141c3c283e73e4
Juan Manuel Arnaiz Caballero <jmanuelarnaiz@gmail.com>
2025-10-29 09:53:49 +0100

    Tantasck installation

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,19 @@
 import {
   ApplicationConfig,
   provideExperimentalZonelessChangeDetection,
-  provideZoneChangeDetection,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import {
+  provideTanStackQuery,
+  QueryClient,
+} from '@tanstack/angular-query-experimental';
+import { withDevtools } from '@tanstack/angular-query-experimental/devtools';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideExperimentalZonelessChangeDetection(),
+    provideTanStackQuery(new QueryClient(), withDevtools()),
   ],
 };
