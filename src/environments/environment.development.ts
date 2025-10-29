faae04102ef73bc6e3bdc632b1db934012fa69d8
Juan Manuel Arnaiz Caballero <jmanuelarnaiz@gmail.com>
2025-10-29 11:04:54 +0100

    Add environments

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  title: 'Get Labels (DEV)',
+  url: 'https://api.github.com/repos/angular/angular/labels',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  title: 'Get Labels',
+  url: 'https://api.github.com/repos/angular/angular/labels',
+};
