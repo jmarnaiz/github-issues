9f7caaee1bdd4d341f0a79b7c4ab1b1520d805c8
Juan Manuel Arnaiz Caballero <jmanuelarnaiz@gmail.com>
2025-10-29 11:06:29 +0100

    WIP: Issues List Page

--- src/app/modules/issues/pages/issues-list/issues-list-page.component.ts
+++ src/app/modules/issues/pages/issues-list/issues-list-page.component.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { IssuesService } from '../../services/issues';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'issues-list-page',
-  imports: [],
+  imports: [RouterLink, JsonPipe],
   templateUrl: './issues-list-page.component.html',
 })
-export default class IssuesListPageComponent {}
+export default class IssuesListPageComponent {
+  issuesService = inject(IssuesService);
+
+  get labelsQuery() {
+    return this.issuesService.labelsQuery;
+  }
+}
